1f46f46e4fd93c4041e5d1ef8859c52043a740b2
aliafrazeh-1378 <109254999+aliafrazeh-1378@users.noreply.github.com>
2022-07-14 23:29:26 +0430

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -46,3 +46,10 @@ self.addEventListener("fetch", (event) => {
         );
       });
 });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
